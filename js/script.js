55af969f3af5ef3815cb10ca8ca73a2cb0ae6c03
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-05-20 19:58:43 -0400

    html and js done

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let weatherData;
+
+const $weatherFor = $('#weather-for');
+const $temperature = $('#temp');
+const $feelsLike = $('#feels-like');
+const $weather = $('#weather');
+
+function render() {
+    $weatherFor.text(weatherData.name);
+    $temperature.text(weatherData.main.temp);
+    $feelsLike.text(weatherData.main.feels_like);
+    $weather.text(weatherData.weather[0].description);
+}
+
+function handleGetWeather(event) {
+    event.preventDefault();
+
+    let searchInfo = $('input#search').val()
+
+    $.ajax({
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${searchInfo}&appid=94cd9592d059de58b137bde5429358af&units=imperial`
+        })
+        .then(
+            function (data) {
+                weatherData = data;
+                render();
+            },
+            function (error) {
+                console.log('bad request: ', error);
+            }
+
+        );
+
+}
+$('form').on('submit', handleGetWeather);
